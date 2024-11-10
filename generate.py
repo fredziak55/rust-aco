223a159c0701913c5b585bf98ef8382a3e2f15c3
fredziak55 <jan.czyz100@gmail.com>
2024-11-10 13:54:51 +0100

    changed point generating to int

--- generate.py
+++ generate.py
@@ -5,8 +5,8 @@ def generate_data(num_rows, max_num):
     with open('dane.txt', 'w') as file:
         file.write(f"{num_rows}\n")
         for i in range(1, num_rows + 1):
-            x = random.uniform(0, max_num)
-            y = random.uniform(0, max_num)
+            x = random.randint(0, max_num)
+            y = random.randint(0, max_num)
             file.write(f"{i} {x} {y}\n")
 
 if __name__ == "__main__":
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     num_rows = int(sys.argv[1])
-    max_num = float(sys.argv[2])
+    max_num = int(sys.argv[2])
 
     generate_data(num_rows, max_num)
     print(f"Generated {num_rows} rows of data with max number {max_num} in dane.txt")
